711ce8e3fdcffcc738a9e515f27041328e7e0fa5
ANIL-SKCH <160247504+ANIL-SKCH@users.noreply.github.com>
2024-04-27 09:12:53 +0530

    Add files via upload

---
+++ CubeProgram.java
@@ -0,0 +1,9 @@
+public class CubeProgram{
+    
+   public static void main(String []args){
+		double num=Double.parseDouble(args[0]);
+                double cube=num*num*num;
+		System.out.println("Cube of the number is " + cube);
+
+	}
+}
